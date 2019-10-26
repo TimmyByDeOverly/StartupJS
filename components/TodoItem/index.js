2070556ee2af05239977056f7f71db00048946fd
TimmyByDeOverly <deyneko.timmi@mail.ru>
2019-10-26 22:48:52 +0300

    fix

--- components/TodoItem/index.js
+++ components/TodoItem/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { View, Text, Button, TextInput } from 'react-native'
 import './index.styl'
 import { observer } from 'startupjs'
@@ -12,38 +12,34 @@ export default observer(function TodoItem(props) {
 
   return pug`
   View.container
-      View.completButton
-          Button(
-              title='Com'
-              color='green'
-              onPress=props.onToggleCompleted
-          )
-          View
-            Button(
-                color='red'
-                title='Del'
-                onPress=props.onDelete
-          )
-          View
-            Button(
-                color='#FFCE00'
-                title='Edit'
-                onPress=toggleEdit
-          )
-      View
-        if editFlag
-            TextInput.textInput(
-            value=input
-            onSubmitEditing=() => props.onEdit(props.title.id, input)
-            onChangeText=input => setInput(input)
-            )
-            if props.onCompletFlag
-                Text.flagCompleted Completed!
-        else
-          View
-            Text.unfulfilledTodo
-                ${props.title}
-            if props.onCompletFlag
-                Text.flagCompleted Completed!
+    View
+      View.navigationButtons
+        Button(
+          title='Complete'
+          onPress=props.onToggleCompleted
+        )
+        Button(
+          title='Delete'
+          onPress=props.onDelete
+        )
+        Button(
+          title='Edit'
+          onPress=toggleEdit
+        )
+    View
+      if editFlag
+        TextInput.textInput(
+          value=input
+          onSubmitEditing=() => props.onEdit(props.title.id, input)
+          onChangeText=input => setInput(input)
+        )
+        if props.onCompletFlag
+          Text.flagCompleted Completed!
+      else
+        View
+          Text.unfulfilledTodo
+            ${props.title}
+          if props.onCompletFlag
+            Text.flagCompleted Completed!
 `
 })
